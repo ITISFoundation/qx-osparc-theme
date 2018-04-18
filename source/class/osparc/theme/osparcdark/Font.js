77b3c667edcf8c59e48d89c1f6bfbce1f95d0d59
Tobias Oetiker <tobi@itis.ethz.ch>
2018-04-18 15:00:55 +0200

    a bit of code reorg and working theme switch as a result

--- source/class/osparc/theme/osparcdark/Font.js
+++ source/class/osparc/theme/osparcdark/Font.js
@@ -16,93 +16,93 @@
     This theme is based in large parts on the the Simple
     theme included with Qooxdoo.
 ************************************************************************ */
-
+/**
+ * @asset(osparc/font/*.woff)
+ * @asset(osparc/font/*.woff2)
+ */
 /**
  * The simple qooxdoo font theme.
  */
-qx.Theme.define("osparc.theme.osparcdark.Font",
-  {
-    fonts :
-  {
-    "default" :
-    {
-      size : 13,
-      family : ["sans-serif"],
+qx.Theme.define("osparc.theme.osparcdark.Font", {
+  fonts: {
+    "default": {
+      size: 13,
+      family: ["sans-serif"],
       color: "text",
       sources: [
         {
           family: "Roboto",
           source: [
-            "osparc/font/roboto-v18-latin-ext_latin-regular.woff2",
-            "osparc/font/roboto-v18-latin-ext_latin-regular.woff"
+            "osparc/font/roboto-v18-latin-ext_latin-regular.woff",
+            "osparc/font/roboto-v18-latin-ext_latin-regular.woff2"
           ]
         }
       ]
     },
 
-    "bold" :
-    {
-      size : 13,
-      family : ["sans-serif"],
-      bold : true,
-      color: "text",
-      sources: [
-        {
-          family: "Roboto",
-          source: [
-            "osparc/font/roboto-v18-latin-ext_latin-700.woff2",
-            "osparc/font/roboto-v18-latin-ext_latin-700.woff"
-          ]
-        }
-      ]
-    },
+    "bold":
+      {
+        size: 13,
+        family: ["sans-serif"],
+        bold: true,
+        color: "text",
+        sources: [
+          {
+            family: "Roboto",
+            source: [
+              "osparc/font/roboto-v18-latin-ext_latin-700.woff",
+              "osparc/font/roboto-v18-latin-ext_latin-700.woff2"
+            ]
+          }
+        ]
+      },
 
-    "headline" :
-    {
-      size : 24,
-      family : ["sans-serif"],
-      color: "text",
-      sources: [
-        {
-          family: "Roboto",
-          source: [
-            "osparc/font/roboto-v18-latin-ext_latin-regular.woff2",
-            "osparc/font/roboto-v18-latin-ext_latin-regular.woff"
-          ]
-        }
-      ]
-    },
+    "headline":
+      {
+        size: 24,
+        family: ["sans-serif"],
+        color: "text",
+        sources: [
+          {
+            family: "Roboto",
+            source: [
+              "osparc/font/roboto-v18-latin-ext_latin-regular.woff",
+              "osparc/font/roboto-v18-latin-ext_latin-regular.woff2"
+            ]
+          }
+        ]
+      },
 
-    "small" :
-    {
-      size : 11,
-      family : ["sans-serif"],
-      color: "text",
-      sources: [
-        {
-          family: "Open Sans",
-          source: [
-            "osparc/font/roboto-v18-latin-ext_latin-regular.woff2",
-            "osparc/font/roboto-v18-latin-ext_latin-regular.woff"
-          ]
-        }
-      ]
-    },
+    "small":
+      {
+        size: 11,
+        family: ["sans-serif"],
+        color: "text",
+        sources: [
+          {
+            family: "Open Sans",
+            source: [
+              "osparc/font/roboto-v18-latin-ext_latin-regular.woff",
+              "osparc/font/roboto-v18-latin-ext_latin-regular.woff2"
+            ]
+          }
+        ]
+      },
 
-    "monospace" :
-    {
-      size : 11,
-      family : ["monospace"],
-      color: "text",
-      sources: [
-        {
-          family: "Open Sans",
-          source: [
-            "osparc/font/roboto-mono-v5-latin-ext_latin-italic.woff2",
-            "osparc/font/roboto-mono-v5-latin-ext_latin-italic.woff"
-          ]
-        }
-      ]
-    }
+    "monospace":
+      {
+        size: 11,
+        family: ["monospace"],
+        color: "text",
+        sources: [
+          {
+            family: "Open Sans",
+            source: [
+              "osparc/font/roboto-mono-v5-latin-ext_latin-italic.woff",
+              "osparc/font/roboto-mono-v5-latin-ext_latin-italic.woff2"
+            ]
+          }
+        ]
+      }
   }
-  });
+});
